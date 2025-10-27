af7c8e1d18434889c5bb130387121ee3d1734510
Numpkens <numpkins1228@gmail.com>
2025-10-27 14:06:16 +0400

    add a help and exit command

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 
@@ -15,14 +21,51 @@ func cleanInput(text string) []string {
 	return words
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0) // Exits the program with a success code
+	return nil // Unreachable code, but satisfies the signature
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	commands := getCommands()
+
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+
+	fmt.Println()
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func startREPL() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
 
 	for {
 		fmt.Fprintf(os.Stderr, "Pokedex > ")
 
 		if !scanner.Scan() {
-			break // Exit loop on EOF or error.
+			break
 		}
 
 		text := scanner.Text()
@@ -34,11 +77,17 @@ func startREPL() {
 
 		commandName := cleaned[0]
 
-		fmt.Printf("Your command was: %s\n", commandName)
+		if command, ok := commands[commandName]; ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("Error executing command %s: %v\n", commandName, err)
+			}
+		} else {
+			fmt.Printf("Unknown command: %s\n", commandName)
+		}
 	}
 }
 
 func main() {
 	startREPL()
 }
-
